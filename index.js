2292ed7cedd0a47127a68ca4181e6a86a3c0ded9
Bawazier <bawaziermurphy@gmail.com>
2023-12-18 20:07:51 +0700

    feat: jarvis with gemini v2.0.0

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const qrcode = require("qrcode-terminal");
-const { Client, LocalAuth } = require("whatsapp-web.js");
 const { body, validationResult } = require("express-validator");
 const express = require("express");
 const cors = require("cors");
 const basicAuth = require("express-basic-auth");
 const { createServer } = require("node:http");
-const { Server } = require("socket.io");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+const { mult } = require("./libs/mult");
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-const port = process.env.PORT || 5003;
 require("dotenv").config();
+const port = process.env.PORT || 5003;
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const env = {
   BOT_NUMBER: process.env.BOT_NUMBER,
@@ -30,65 +30,93 @@ app.use(
   })
 );
 
-const client = new Client({
-  puppeteer: {
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  },
-  authStrategy: new LocalAuth({
-    clientId: "client-one",
-  }),
-});
-
-client.initialize();
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("authenticated", (session) => {
-  console.log("WHATSAPP WEB => Authenticated");
-});
-
-client.on("ready", () => {
-  console.log("Client is ready!");
-});
-
-client.on("message", async (message) => {
-  const isFromBot = message.from === env.BOT_NUMBER;
-  const text = message.body;
-  console.log(message.from);
-  if (isFromBot) {
-    io.emit("answer", {
-      answer: text,
-    });
-  }
-});
-
 app.get("/", (req, res) => {
-  res.send("Jarvis Engine Ready v1.0.1");
+  res.send("Jarvis Engine Ready v2.0.0");
 });
 
-app.post("/ask", body("q").notEmpty().isString().trim(), async (req, res) => {
-  const { q } = req.body;
-  const result = validationResult(req);
-  if (result.isEmpty()) {
-    try {
-      await client.sendMessage(env.BOT_NUMBER, `${q} [ANSWER IN ENGLISH]`);
-      return res.send({ status: "ok" });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).send(err);
+app.post(
+  "/ask",
+  mult,
+  body("q").notEmpty().isString().trim(),
+  async (req, res) => {
+    const { q } = req.body;
+    const files = req.files;
+    const hasFiles =
+      files !== null ? (files.length >= 1 ? true : false) : false;
+    let aiModel = "gemini-pro";
+    if (hasFiles) {
+      aiModel = "gemini-pro-vision";
     }
-  } else {
-    return res.status(400).send({ errors: result.array() });
+    const generationConfig = {
+      temperature: 1,
+      topP: 1,
+      topK: 1,
+    };
+    const model = genAI.getGenerativeModel({
+      model: aiModel,
+      generationConfig,
+    });
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      try {
+        if (hasFiles) {
+          const imageParts = filesToGenerative(files);
+          const result = await model.generateContent([q, ...imageParts]);
+          const response = result.response;
+          const text = response.text();
+          unlinkFiles(files);
+          return res.send({ status: "ok", asnwer: text });
+        } else {
+          const chat = model.startChat();
+          const result = await chat.sendMessage(q);
+          const response = result.response;
+          const text = response.text();
+          unlinkFiles(files);
+          return res.send({ status: "ok", asnwer: text });
+        }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }
+    } else {
+      return res.status(400).send({ errors: result.array() });
+    }
   }
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
+);
 
 server.listen(port, () => {
-  console.log(`Jarvis v1.0.1 Port Listen on ${port}`);
+  console.log(`Jarvis v2.0.0 Port Listen on ${port}`);
 });
+
+const unlinkFiles = (files) => {
+  try {
+    for (let i in files) {
+      const file = files[i];
+      fs.unlink(file.path, (err) => {
+        if (err) throw err;
+        console.log("deleted " + file.path);
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const filesToGenerative = (files) => {
+  const generatives = [];
+  for (let i in files) {
+    const file = files[i];
+    generatives.push(fileToGenerativePart(file.path, file.mimetype));
+  }
+
+  return generatives;
+};
+
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
---
+++ libs/mult.js
@@ -0,0 +1,66 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      cb(null, `${Date.now()}${ext}`);
+    },
+  }),
+});
+
+const mult = (req, res, next) => {
+  // Use multer upload instance
+  upload.array("files", 5)(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+
+    // Retrieve uploaded files
+    const files = req.files;
+    const errors = [];
+
+    if (!files) {
+      next();
+      return;
+    }
+
+    // Validate file types and sizes
+    files.forEach((file) => {
+      const allowedTypes = ["image/jpeg", "image/png"];
+      const maxSize = 4 * 1024 * 1024; // 4MB
+
+      if (!allowedTypes.includes(file.mimetype)) {
+        errors.push(`Invalid file type: ${file.originalname}`);
+      }
+
+      if (file.size > maxSize) {
+        errors.push(`File too large: ${file.originalname}`);
+      }
+    });
+
+    // Handle validation errors
+    if (errors.length > 0) {
+      // Remove uploaded files
+      files.forEach((file) => {
+        fs.unlinkSync(file.path);
+      });
+
+      return res.status(400).json({ errors });
+    }
+
+    // Attach files to the request object
+    req.files = files;
+
+    // Proceed to the next middleware or route handler
+    next();
+    return;
+  });
+};
+
+module.exports = { mult };
